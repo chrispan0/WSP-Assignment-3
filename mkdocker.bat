c7d71b92ff6dbb122edff628a16825559bcea628
chrispan0 <cv.panetta@gmail.com>
2024-11-13 09:44:52 -0500

    Update mkdocker.bat

--- mkdocker.bat
+++ mkdocker.bat
@@ -2,6 +2,6 @@ git fetch
 git pull
 docker stop wsp-assignment-3
 docker rm wsp-assignment-3
-cd C:\Users\Administrator\Documents\WSP-Assignment-3\WSP-Assignment-3
+cd C:\Users\Administrator\Documents\GitHub\WSP-Assignment-3\WSP-Assignment-3
 docker build -t wsp-assignment-3 .
 docker run -d -p 1447:4000 --name wsp-assignment-3 wsp-assignment-3:latest
